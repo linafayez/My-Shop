0f2ee7add7ebdd6cd97ca4791a243a9308545be6
Lina Taweel <47228614+linafayez@users.noreply.github.com>
2020-11-02 22:42:10 +0200

    order view

--- app/src/main/java/com/shop/myshop/Admin/OrderView.java
+++ app/src/main/java/com/shop/myshop/Admin/OrderView.java
@@ -1,29 +1,54 @@
 package com.shop.myshop.Admin;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.shop.myshop.OrderModel;
+import com.shop.myshop.ProductsModel;
 import com.shop.myshop.R;
 import com.shuhart.stepview.StepView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrderView extends Fragment {
+public class OrderView extends Fragment{
     StepView stepView;
+    RecyclerView items;
+    ArrayList<ProductsModel> data;
+    MapView mapView;
+    GoogleMap googleMap;
+    TextView OrderId, userId;
+    OrderModel orderModel;
+
     public OrderView() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -34,7 +59,18 @@ public class OrderView extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-      stepView= view.findViewById(R.id.step_view);
+        OrderId = view.findViewById(R.id.id);
+        items = view.findViewById(R.id.orderItems);
+        userId = view.findViewById(R.id.UserId);
+        orderModel = OrderViewArgs.fromBundle(getArguments()).getOrder();
+        OrderId.setText(orderModel.getOrderId());
+        userId.setText("UserId:" + orderModel.getUserId().substring(0, 10));
+        stepView = view.findViewById(R.id.step_view);
+        data = orderModel.getProductsModels();
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext());
+        items.setLayoutManager(manager);
+        items.setHasFixedSize(false);
+   //     items.setAdapter(adapter);
         List<String> step = new ArrayList<>();
         step.add("Step 1");
         step.add("step 2");
@@ -43,7 +79,7 @@ public class OrderView extends Fragment {
                 .selectedTextColor(ContextCompat.getColor(getContext(), R.color.colorAccent))
                 .animationType(StepView.ANIMATION_CIRCLE)
                 .selectedCircleColor(ContextCompat.getColor(getContext(), R.color.colorAccent))
-             //   .selectedCircleRadius(getResources().getDimensionPixelSize(R.dimen.dp14))
+                //   .selectedCircleRadius(getResources().getDimensionPixelSize(R.dimen.dp14))
                 .selectedStepNumberColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
                 // You should specify only stepsNumber or steps array of strings.
                 // In case you specify both steps array is chosen.
@@ -53,9 +89,9 @@ public class OrderView extends Fragment {
                 .stepsNumber(step.size())
 
                 .animationDuration(getResources().getInteger(android.R.integer.config_shortAnimTime))
-               // .stepLineWidth(getResources().getDimensionPixelSize(R.dimen.dp1))
-              //  .textSize(getResources().getDimensionPixelSize(R.dimen.sp14))
-               // .stepNumberTextSize(getResources().getDimensionPixelSize(R.dimen.sp16))
+                // .stepLineWidth(getResources().getDimensionPixelSize(R.dimen.dp1))
+                //  .textSize(getResources().getDimensionPixelSize(R.dimen.sp14))
+                // .stepNumberTextSize(getResources().getDimensionPixelSize(R.dimen.sp16))
                 .typeface(ResourcesCompat.getFont(getContext(), R.font.roboto_light))
                 // other state methods are equal to the corresponding xml attributes
                 .commit();
@@ -63,4 +99,5 @@ public class OrderView extends Fragment {
 
 
     }
+
 }
---
+++ app/src/main/java/com/shop/myshop/Admin/OrderViewMap.java
@@ -0,0 +1,7 @@
+package com.shop.myshop.Admin;
+
+import com.google.android.gms.maps.GoogleMap;
+
+public interface OrderViewMap {
+    void onMapReady(GoogleMap googleMap);
+}
